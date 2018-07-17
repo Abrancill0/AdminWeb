fc7a25d17441da041c874e49ffcf10f7ee948cb6
Abrancill0 <38171762+Abrancill0@users.noreply.github.com>
2018-07-17 10:40:42 -0500

    cambio 5

--- SCGESP/Controllers/APP/ListaAutorizacionAdministrativaController.cs
+++ SCGESP/Controllers/APP/ListaAutorizacionAdministrativaController.cs
@@ -45,7 +45,7 @@ namespace SCGESP.Controllers
                 Operacion = 1,
             };
 
-            entrada.agregaElemento("Estatus", 2);
+            entrada.agregaElemento("estatus", 2);
 
             DataTable DTListaAdministrativos = new DataTable();
 
