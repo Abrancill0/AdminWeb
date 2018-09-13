86d595fe071a5e3d826ff5ef58f3e7e45dc50874
Abraham <abrx23@gmail.com>
2018-09-12 20:41:54 -0500

    Pendientes
    Requsisicones pendientes
    OC Vobo Pendientes
    OC Admo Pendientes

---
+++ SCGESP/Controllers/APP/NumeroOrdenCompraVoboController.cs
@@ -0,0 +1,93 @@
+﻿using Ele.Generales;
+using SCGESP.Clases;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.Http;
+using System.Xml;
+
+namespace SCGESP.Controllers.APP
+{
+	public class NumeroOrdenCompraVoboController : ApiController
+	{
+		public class Datos
+		{
+			public string Usuario { get; set; }
+			public string Origen { get; set; }
+		}
+
+		public class NumeroOrdenesVoboResult
+		{
+			public string Tipo { get; set; }
+			public int NumeroOrdenesVobo { get; set; }
+
+		}
+
+		public List<NumeroOrdenesVoboResult> Post(Datos Datos)
+		{
+			string UsuarioDesencripta = Seguridad.DesEncriptar(Datos.Usuario);
+
+			DocumentoEntrada entrada = new DocumentoEntrada
+			{
+				Usuario = UsuarioDesencripta,
+				Origen = "AdminAPP",
+				Transaccion = 120768,
+				Operacion = 1,
+			};
+
+			//entrada.agregaElemento("estatus", 1);
+			entrada.agregaElemento("estatus", 1);
+
+			DocumentoSalida respuesta = PeticionCatalogo(entrada.Documento);
+
+			DataTable DTListaVobo = new DataTable();
+
+			if (respuesta.Resultado == "1")
+			{
+				DTListaVobo = respuesta.obtieneTabla("Catalogo");
+
+				int NumOCVobo = DTListaVobo.Rows.Count;
+
+				List<NumeroOrdenesVoboResult> lista = new List<NumeroOrdenesVoboResult>();
+
+				NumeroOrdenesVoboResult ent = new NumeroOrdenesVoboResult
+				{
+					Tipo = "Ordenes Vobo Pendientes",
+					NumeroOrdenesVobo = NumOCVobo
+
+				};
+				lista.Add(ent);
+
+				return lista;
+			}
+			else
+			{
+				List<NumeroOrdenesVoboResult> lista = new List<NumeroOrdenesVoboResult>();
+
+				NumeroOrdenesVoboResult ent = new NumeroOrdenesVoboResult
+				{
+					Tipo = "Ordenes Vobo Pendientes",
+					NumeroOrdenesVobo = 0
+
+				};
+				lista.Add(ent);
+
+				return lista;
+			}
+
+		}
+
+
+		public static DocumentoSalida PeticionCatalogo(XmlDocument doc)
+		{
+			Localhost.Elegrp ws = new Localhost.Elegrp
+			{
+				Timeout = -1
+			};
+			string respuesta = ws.PeticionCatalogo(doc);
+			return new DocumentoSalida(respuesta);
+		}
+
+
+	}
+}
---
+++ SCGESP/Controllers/APP/NumeroRequisicionesPendientesController.cs
@@ -0,0 +1,92 @@
+﻿using Ele.Generales;
+using SCGESP.Clases;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.Http;
+using System.Xml;
+
+namespace SCGESP.Controllers.APP
+{
+    public class NumeroRequisicionesPendientesAppController : ApiController
+    {
+        public class Datos
+        {
+            public string Usuario { get; set; }
+            public string Origen { get; set; }
+        }
+
+        public class NUmeroRequisicionesResult
+		{
+            public string Tipo { get; set; }
+			public int NumeroRequisiciones { get; set; }
+
+		}
+
+        public List<NUmeroRequisicionesResult> Post(Datos Datos)
+        {
+            string UsuarioDesencripta = Seguridad.DesEncriptar(Datos.Usuario);
+
+            DocumentoEntrada entrada = new DocumentoEntrada
+            {
+                Usuario = UsuarioDesencripta,
+                Origen = "AdminApp",  
+                Transaccion = 120760,
+                Operacion = 1
+            };
+
+            entrada.agregaElemento("proceso", "2");
+
+            DocumentoSalida respuesta = PeticionCatalogo(entrada.Documento);
+
+            DataTable DTRequisiciones = new DataTable();
+
+            if (respuesta.Resultado == "1")
+            {
+                DTRequisiciones = respuesta.obtieneTabla("Catalogo");
+
+				int NumReq = DTRequisiciones.Rows.Count;
+
+                List<NUmeroRequisicionesResult> lista = new List<NUmeroRequisicionesResult>();
+				
+					NUmeroRequisicionesResult ent = new NUmeroRequisicionesResult
+					{
+                        Tipo = "Requisiciones Pendientes",
+						NumeroRequisiciones = NumReq
+
+					};
+                    lista.Add(ent);
+                
+                return lista;
+            }
+            else
+            {
+				List<NUmeroRequisicionesResult> lista = new List<NUmeroRequisicionesResult>();
+
+				NUmeroRequisicionesResult ent = new NUmeroRequisicionesResult
+				{
+					Tipo = "Requisiciones Pendientes",
+					NumeroRequisiciones = 0
+
+				};
+				lista.Add(ent);
+
+				return lista;
+            }
+
+        }
+
+
+        public static DocumentoSalida PeticionCatalogo(XmlDocument doc)
+        {
+            Localhost.Elegrp ws = new Localhost.Elegrp
+            {
+                Timeout = -1
+            };
+            string respuesta = ws.PeticionCatalogo(doc);
+            return new DocumentoSalida(respuesta);
+        }
+
+
+    }
+}
