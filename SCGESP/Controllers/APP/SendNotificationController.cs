e27f4717944f852143712dd107ec6382d3857587
Abraham <abrx23@gmail.com>
2018-09-17 12:27:11 -0500

    Adecuacion de Api envio notificacione
    Se agregaron los parametros que se requieren para el envio de notificaciones

--- SCGESP/Controllers/APP/SendNotificationController.cs
+++ SCGESP/Controllers/APP/SendNotificationController.cs
@@ -8,7 +8,11 @@ namespace SCGESP.Controllers.APP
         public class datos
         {
             public string usuario { get; set; }
-            public string contrasena { get; set; }
+            public string TokenID { get; set; }
+            public string Titulo { get; set; }
+            public string Mensaje { get; set; }
+            
+                
         }
 
         public class ObtieneEmpleadoResult
@@ -16,14 +20,14 @@ namespace SCGESP.Controllers.APP
             public int SgUsuEmpleado { get; set; }
         }
 
-        public static String SendNotificationFromFirebaseCloud()
+        public static String SendNotificationFromFirebaseCloud(datos Datos)
         {
             using (var firebase = new FireBase.Notification.Firebase())
             {
                 firebase.ServerKey = "AAAASvHYA78:APA91bEYxMsqdhV-7h-DdDGORTinWDc8G_JEYmOBhMA7FVNfNNpbrsviDW0BNK0etgD2l6QEMiiQHz3Of_Kv2YMEwQHVl6kvoStC0SBucb9nSGP6XGWG-6IAN48WBtb4Te2QPG1jWzMx";
-                var id = "{Your Device Id}";
-                firebase.PushNotifyAsync(id, "Hello", "World").Wait();
-                Console.ReadLine();
+                var id = Datos.TokenID;
+                firebase.PushNotifyAsync(id,Datos.Titulo, Datos.Mensaje).Wait();
+                //Console.ReadLine();
             }
 
             return "OK";
