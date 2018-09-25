508a536aa7b4209780caa9da1c2601f9544c8487
Abraham <abrx23@gmail.com>
2018-09-25 08:47:47 -0500

    Try catch en autoriza informes

--- SCGESP/Controllers/EleAPI/AutorizaInformeController.cs
+++ SCGESP/Controllers/EleAPI/AutorizaInformeController.cs
@@ -39,61 +39,72 @@ namespace SCGESP.Controllers.EleAPI
 
         public string Post(datos Datos)
         {
-            string usuariodesencripta = Seguridad.DesEncriptar(Datos.Usuario);
-
-            SqlCommand comando = new SqlCommand("AutorizaInformes");
-            comando.CommandType = CommandType.StoredProcedure;
-
-            //Declaracion de parametros
-            comando.Parameters.Add("@i_id", SqlDbType.Int);
-			comando.Parameters.Add("@Usuario", SqlDbType.VarChar);
-			comando.Parameters.Add("@comentario", SqlDbType.VarChar);
-
-
-			//Asignacion de valores a parametros
-			comando.Parameters["@i_id"].Value = Datos.idinforme;
-			comando.Parameters["@Usuario"].Value = usuariodesencripta;
-			comando.Parameters["@comentario"].Value = Datos.comentario;
-
-			comando.Connection = new SqlConnection(VariablesGlobales.CadenaConexion);
-            comando.CommandTimeout = 0;
-            comando.Connection.Open();
-            //DA.SelectCommand = comando;
-            // comando.ExecuteNonQuery();
-
-            DataTable DT = new DataTable();
-            SqlDataAdapter DA = new SqlDataAdapter(comando);
-            comando.Connection.Close();
-            DA.Fill(DT);
-
-            if (DT.Rows.Count > 0)
+            try
             {
+                string usuariodesencripta = Seguridad.DesEncriptar(Datos.Usuario);
 
-                foreach (DataRow row in DT.Rows)
+                SqlCommand comando = new SqlCommand("AutorizaInformes");
+                comando.CommandType = CommandType.StoredProcedure;
+
+                //Declaracion de parametros
+                comando.Parameters.Add("@i_id", SqlDbType.Int);
+                comando.Parameters.Add("@Usuario", SqlDbType.VarChar);
+                comando.Parameters.Add("@comentario", SqlDbType.VarChar);
+
+
+                //Asignacion de valores a parametros
+                comando.Parameters["@i_id"].Value = Datos.idinforme;
+                comando.Parameters["@Usuario"].Value = usuariodesencripta;
+                comando.Parameters["@comentario"].Value = Datos.comentario;
+
+                comando.Connection = new SqlConnection(VariablesGlobales.CadenaConexion);
+                comando.CommandTimeout = 0;
+                comando.Connection.Open();
+                //DA.SelectCommand = comando;
+                // comando.ExecuteNonQuery();
+
+                DataTable DT = new DataTable();
+                SqlDataAdapter DA = new SqlDataAdapter(comando);
+                comando.Connection.Close();
+                DA.Fill(DT);
+
+                if (DT.Rows.Count > 0)
                 {
-                    string mensaje = Convert.ToString(row["msn"]);
-                    string titulo = Convert.ToString(row["titulo"]);
-                    string autorizador = Convert.ToString(row["a_uautoriza"]);
-					int autorizador_final = Convert.ToInt16(row["a_autorizador_final"]);
-					if (autorizador_final == 1) {
-						titulo = "Respuesta de solicitud para autorización para el descuento vía nómina de la requisición #" + Datos.idrequisicion;
-						string body_mensaje = Mensaje(autorizador, Datos.idrequisicion, Datos.comentario_respuesta, Datos.usuario_fecha_responde);
-						EnvioCorreosELE.Envio(usuariodesencripta, "", "", autorizador, "", titulo, body_mensaje, 0);
-					}
-					else
-					{
-						EnvioCorreosELE.Envio(usuariodesencripta, "", "", autorizador, "", titulo, mensaje, 0);
-					}
+
+                    foreach (DataRow row in DT.Rows)
+                    {
+                        string mensaje = Convert.ToString(row["msn"]);
+                        string titulo = Convert.ToString(row["titulo"]);
+                        string autorizador = Convert.ToString(row["a_uautoriza"]);
+                        int autorizador_final = Convert.ToInt16(row["a_autorizador_final"]);
+                        if (autorizador_final == 1)
+                        {
+                            titulo = "Respuesta de solicitud para autorización para el descuento vía nómina de la requisición #" + Datos.idrequisicion;
+                            string body_mensaje = Mensaje(autorizador, Datos.idrequisicion, Datos.comentario_respuesta, Datos.usuario_fecha_responde);
+                            EnvioCorreosELE.Envio(usuariodesencripta, "", "", autorizador, "", titulo, body_mensaje, 0);
+                        }
+                        else
+                        {
+                            EnvioCorreosELE.Envio(usuariodesencripta, "", "", autorizador, "", titulo, mensaje, 0);
+                        }
+
+                    }
 
                 }
 
+                List<ObtieneInformeResult> lista = new List<ObtieneInformeResult>();
+
+                //if (DT.Rows.Count > 0)
+                //{ }
+                return "";
+            }
+            catch (Exception ex)
+            {
+
+                return ex.ToString();
             }
 
-            List<ObtieneInformeResult> lista = new List<ObtieneInformeResult>();
-
-            //if (DT.Rows.Count > 0)
-            //{ }
-            return "";
+            
         }
 
 		public static string Mensaje(string usuario_destino, string idrequisicion, string comentario, string usuario_responde)
