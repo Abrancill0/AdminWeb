0e746ede6955fc447d70378f4c53eb09a68c5a57
Abraham <abrx23@gmail.com>
2018-09-24 21:38:44 -0500

    Adecuacion baja Alterno

--- SCGESP/Controllers/APP/BajaAlternoController.cs
+++ SCGESP/Controllers/APP/BajaAlternoController.cs
@@ -19,7 +19,7 @@ namespace SCGESP.Controllers
         }
 
         //public List<ObtieneParametrosSalida> Post(ParametrosEntrada Datos)
-        public XmlDocument Post(ParametrosEntrada Datos)
+        public DocumentoSalida Post(ParametrosEntrada Datos)
         {
             DocumentoEntrada entrada = new DocumentoEntrada
             {
@@ -35,13 +35,12 @@ namespace SCGESP.Controllers
 
             if (respuesta.Resultado == "1")
             {
-                return respuesta.Documento;
+                return respuesta;
             }
             else
             {
-                var errores = respuesta.Errores;
-
-                return null;
+                //var errores = respuesta.Errores;
+                return respuesta;
             }
         }
 
